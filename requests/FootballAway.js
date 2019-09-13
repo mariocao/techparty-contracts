6c4a65e96c146d2d47ba837ce8cccef1bb8c1d41
Mario Cao <mariocao@gmail.com>
2019-09-13 21:04:34 +0200

    refactor: update data requets

--- requests/FootballAway.js
+++ requests/FootballAway.js
@@ -1,14 +1,14 @@
 import { Aggregator, Request, Source, Tally, Types } from "witnet-requests"
 const { REDUCERS } = Types
 
-const web365 = new Source("https://webws.365scores.com/web/game/?gameId=2088066")
+const web365 = new Source("https://webws.365scores.com/web/game/?gameId=2034921")
   .parseJSON().asMap()
   .get("game").asMap()
   .get("awayCompetitor").asMap()
   .get("score").asFloat()
   .round()
 
-const sofascore = new Source("https://www.sofascore.com/event/8397714/json")
+const sofascore = new Source("https://www.sofascore.com/event/8280731/json")
   .parseJSON().asMap()
   .get("event").asMap()
   .get("awayScore").asMap()
@@ -30,8 +30,8 @@ const request = new Request()
   .addSource(web365)
   .setAggregator(aggregator)
   .setTally(tally)
-  .setFees(10, 1, 1, 1)
-  .setQuorum(1, 1)
-  // .schedule(1669852800)
+  .setFees(11, 1, 1, 1)
+  .setQuorum(2, 1)
+  .schedule(1568460000)
 
 export { request as default }
