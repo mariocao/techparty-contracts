f8bbcf5d7893542fa9d94a314a0e3784a0861c3a
Mario Cao <mariocao@gmail.com>
2019-09-13 19:28:38 +0200

    fix: fix RADON data requests

--- requests/FootballHome.js
+++ requests/FootballHome.js
@@ -1,11 +1,12 @@
 import { Aggregator, Request, Source, Tally, Types } from "witnet-requests"
 const { REDUCERS } = Types
 
-const web365 = new Source("https://webws.365scores.com/web/game/?gameId=2088066").asString()
+const web365 = new Source("https://webws.365scores.com/web/game/?gameId=2088066")
   .parseJSON().asMap()
   .get("game").asMap()
   .get("homeCompetitor").asMap()
   .get("score").asFloat()
+  .round()
 
 const sofascore = new Source("https://www.sofascore.com/event/8397714/json")
   .parseJSON().asMap()
@@ -26,8 +27,11 @@ const tally = new Tally(aggregator)
 
 const request = new Request()
   .addSource(sofascore)
+  .addSource(web365)
   .setAggregator(aggregator)
   .setTally(tally)
+  .setFees(10, 1, 1, 1)
+  .setQuorum(1, 1)
   // .schedule(1669852800)
 
 export { request as default }
